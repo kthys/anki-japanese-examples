f0233e2965efe2b5d681a286bd7dd890c9f3da7d
kthys <32138080+kthys@users.noreply.github.com>
2026-02-21 16:38:53 +0100

    feat: implement an automatic changelog display on addon update.

--- __init__.py
+++ __init__.py
@@ -1,4 +1,6 @@
-from . import GUI, japanese_examples, config_ui
-from aqt import mw
+from . import GUI, japanese_examples, config_ui, changelog
+from aqt import mw, gui_hooks
 
 mw.addonManager.setConfigAction(__name__, config_ui.on_config)
+
+gui_hooks.main_window_did_init.append(lambda: changelog.check_and_show_changelog(__name__))
